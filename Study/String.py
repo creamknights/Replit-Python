f2d57ffd228ebc015a699e670ba3b2b959df3085
creamknights <144908571+creamknights@users.noreply.github.com>
2024-07-21 14:00:55 +0000

    end=" "

--- Study/String.py
+++ Study/String.py
@@ -23,3 +23,37 @@ numbers = [1, 2, 3]
 result = ', '.join(map(str, numbers))
 print(result)
 # 출력: 1, 2, 3
+"""
+replace
+문자열 내의 특정 부분 문자열을 다른 문자열로 대체
+대체된 새로운 문자열을 반환
+"""
+
+a = 'nice to meet you'
+# 모음 a, e, i, o, u 제거
+result = a.replace("a",
+                   "").replace("e",
+                               "").replace("i",
+                                           "").replace("o",
+                                                       "").replace("u", "")
+print(result)  # nc t mt y
+
+# 정규 표현식으로 모음 a, e, i, o, u 제거
+import re
+
+a = 'nice to meet you'
+# 정규 표현식을 사용하여 모음 제거
+result = re.sub(r'[aeiou]', '', a)  # 문자열에서 a, e, i, o, u 중 하나라도 일치하는 부분을 찾음
+print(result)  # nc t mt y
+
+# 고양이 출력하기
+print('\\    /\\')  # 백슬래쉬 \ 출력을 위해 앞에 \ 붙여줌
+print(" )  ( ')")  # 작은 따옴표 출력을 위해 큰 따옴표로 감싸줌
+print('(  /  )')
+print(" \\(__)|")
+
+# print() end
+# end=' '로 설정하면 줄 바꿈 대신 공백을 출력
+print("a", end=" ")
+print("b")
+# 출력: a b
