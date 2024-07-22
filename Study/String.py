7c4682e5d3f898a6bdf8d94c7152140f52dbff73
creamknights <144908571+creamknights@users.noreply.github.com>
2024-07-22 06:23:08 +0000

    string

--- Study/String.py
+++ Study/String.py
@@ -22,6 +22,15 @@ print(result)  # 출력: abc
 numbers = [1, 2, 3]
 result = ' '.join(map(str, numbers))
 print(result)  # 출력: 1 2 3
+
+# 백준 10810번
+n, m = map(int, input().split())  # 1번부터 n번이 적힌 n개의 바구니
+array = [0] * n
+for _ in range(m):
+            i, j, k = map(int, input().split())
+            # i번부터 j번 바구니에 k번이 적힌 공을 넣는다.
+            array[i - 1:j] = [k] * (j - i + 1)
+print(" ".join(map(str, array)))  # join() 메서드는 문자열에만 적용할 수 있으므로 문자열로 변환 후 사용
 """
 replace
 문자열 내의 특정 부분 문자열을 다른 문자열로 대체
